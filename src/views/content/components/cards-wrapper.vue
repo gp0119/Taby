6fbeaebb672383c4fec52f455fe383ade98d31bd
ganpeng <190875565@qq.com>
2025-03-12 12:48:27 +0800

    feat: trim favicon input before adding to ensure clean data submission

--- src/views/content/components/cards-wrapper.vue
+++ src/views/content/components/cards-wrapper.vue
@@ -143,7 +143,7 @@ function onEdit(child: CardWithFavicon) {
     onPositiveClick: async () => {
       let faviconId
       if (formModel.value.favicon) {
-        faviconId = await dataManager.addFavicon(formModel.value.favicon)
+        faviconId = await dataManager.addFavicon(formModel.value.favicon.trim())
       }
       await dataManager.updateCard(child.id, {
         title: formModel.value.title,
